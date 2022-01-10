e227d6daa38939c98923969ca3574c51ebb578fe
harini <mansaniharini@gmail.com>
2022-01-10 20:47:46 +0530

    second commit

--- src/gitdemo/demo2.java
+++ src/gitdemo/demo2.java
@@ -1,10 +1,10 @@
 package gitdemo;
 
-public class demo {
+public class demo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello123");
+
 	}
 
 }
